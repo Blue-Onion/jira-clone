1d1548c0ee07b237b3faa5c966040f72faf4e08c
Blue Onion <rawatadityasingh963@gmail.com>
2025-04-22 22:28:41 +0530

    Adding Nesscary files

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,24 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import Link from 'next/link'
-import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
     <header>
-        <nav className="">
-            <Link href="/">
-
-            </Link>
-        </nav>
+      <nav className="">
+        <Link href="/">
+          <Image src={"/logo2.png"} alt="logo" width={200} height={40} />
+        </Link>
+      </nav>
       <SignedOut>
-        <SignInButton>
-            sign IN Bitcgh
-        </SignInButton>
+        <SignInButton>sign IN Bitcgh</SignInButton>
       </SignedOut>
       <SignedIn>
-        <UserButton  />
-        </SignedIn>    
-        </header>
-  )
-}
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
